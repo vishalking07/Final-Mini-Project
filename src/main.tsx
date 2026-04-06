0b451a8acd8c048f2f7a6b3f1cc56c0a29d2eca9
vishalo7 <spideyvishal05@gmail.com>
2026-04-06 14:50:20 +0530

    Revert Google Client ID to correct value

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "147666169378-sbabf642o443qpg4dqsnf739c5952a.apps.googleusercontent.com";
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "147666169378-sbabf642o443qpg4dqsnf739c5952avg.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={googleClientId}>
